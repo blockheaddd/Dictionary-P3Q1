5dcfd0105ff7167b7df112179cce0cad1fa729bb
Gus Silva <gus7silva@yahoo.com>
2015-05-13 14:27:43 -0700

    05/10 - Add KVPair to GitHub

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created by GusSilva on 5/13/15.
+ */
+public class Test {
+    private int k;
+    private String e;
+    /** Constructors */
+    KVpair(int kval, String eval)
+    { k = kval; e = eval; }
+    /** Data member access functions */
+    public int key() { return k; }
+    public String value() { return e; }
+}
